297a90c066d8e09aac5f42879a9752bd562d3472
Prajwal-audacious <prajwal.ramanwal@techiegent.com>
2023-03-14 13:59:03 +0530

    feat/components

---
+++ src/component/Second.js
@@ -0,0 +1,11 @@
+export let Second = ()=>{
+      return (
+            <h1>this is second.js</h1>
+      )
+}
+
+export let Third = ()=>{
+      return (
+            <h2>this is third component</h2>
+      )
+}
